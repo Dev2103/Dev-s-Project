99f76f128220f408bf01c29bc8be91440170cb72
Dev Nahar <Dev@Devs-iMac.home>
2018-08-08 13:53:00 +0100

    bullets, guns sprite ready. building destruction half done.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import sys
 SILVER = (192,192,192)	# FOR GUN 1 BULLET
 GOLD = (255,215,0)		# FOR GUN 2 BULLET
 BLUE = (18,198,236)		# FOR GUN 1 BARREL
-GOLDISH = (234,154,0)	# FOR GUN 2 B
+GOLDISH = (234,154,0)	# FOR GUN 2 BARREL
 
 
 pygame.init()
@@ -32,20 +32,20 @@ meteor4_speed = 1.7
 
 # Game Assets
 tall1 = pygame.transform.scale(pygame.image.load("img/tall1.png"), [65,170])#790
-tall1d1 = pygame.transform.scale(pygame.image.load("img/tall1d1.png"), [65,145])
-tall1d2 = pygame.transform.scale(pygame.image.load("img/tall1d2.png"), [65,83])
+tall1d1 = pygame.transform.scale(pygame.image.load("img/tall1d1.png"), [65,170])
+tall1d2 = pygame.transform.scale(pygame.image.load("img/tall1d2.png"), [65, 170])
 
 tall2 = pygame.transform.scale(pygame.image.load("img/tall2.png"), [125,196])#764
-tall2d1 = pygame.transform.scale(pygame.image.load("img/tall2d1.png"), [125,179])#790
-tall2d2 = pygame.transform.scale(pygame.image.load("img/tall2d2.png"), [125,105])#790
+tall2d1 = pygame.transform.scale(pygame.image.load("img/tall2d1.png"), [125,196])#790
+tall2d2 = pygame.transform.scale(pygame.image.load("img/tall2d2.png"), [125,196])#790
 
 tall3 = pygame.transform.scale(pygame.image.load("img/tall3.png"), [125,196])#764
-tall3d1 = pygame.transform.scale(pygame.image.load("img/tall3d1.png"), [125,166])#790
-tall3d2 = pygame.transform.scale(pygame.image.load("img/tall3d2.png"), [125,109])#790
+tall3d1 = pygame.transform.scale(pygame.image.load("img/tall3d1.png"), [125,196])#790
+tall3d2 = pygame.transform.scale(pygame.image.load("img/tall3d2.png"), [125,196])#790
 
 medium1 = pygame.transform.scale(pygame.image.load("img/medium1.png"), [116,140])#820
-medium1d1 = pygame.transform.scale(pygame.image.load("img/medium1d1.png"), [116,125])#790
-medium1d2 = pygame.transform.scale(pygame.image.load("img/medium1d2.png"), [116,71])#790
+medium1d1 = pygame.transform.scale(pygame.image.load("img/medium1d1.png"), [116,140])#790
+medium1d2 = pygame.transform.scale(pygame.image.load("img/medium1d2.png"), [116,140])#790
 
 medium2 = pygame.transform.scale(pygame.image.load("img/medium2.png"), [116,140]) #820
 medium2d1 = pygame.transform.scale(pygame.image.load("img/medium2d1.png"), [116,124])#790
@@ -69,8 +69,6 @@ meteor1 = pygame.transform.scale(pygame.image.load("img/meteor1.png"), [25,49])
 meteor2 = pygame.transform.scale(pygame.image.load("img/meteor2.png"), [40,66])
 meteor3 = pygame.transform.scale(pygame.image.load("img/meteor3.png"), [20,52])
 meteor4 = pygame.transform.scale(pygame.image.load("img/meteor4.png"), [45,78])
-
-
 
 gun1 = pygame.transform.scale(pygame.image.load("img/gun1.png"), [80,38])
 gun2 = pygame.transform.scale(pygame.image.load("img/gun2.png"), [80,42])
@@ -183,14 +181,66 @@ for x in range (no_Meteor4):
 
 allBuildings = pygame.sprite.Group()
 
+class Tall1(pygame.sprite.Sprite):
+	def __init__(self, width, height, x, y):
+		super().__init__()
+		self.image = pygame.Surface([width, height])
+		self.image = tall1
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+tall1_group = pygame.sprite.Group()
+tall1_sprite1 = Tall1(125, 196, 322, 790)
+tall1_sprite2 = Tall1(116, 140, 787, 790)
+tall1_group.add(tall1_sprite1)
+tall1_group.add(tall1_sprite2)
+allBuildings.add(tall1_sprite1)
+allBuildings.add(tall1_sprite2)
+
 class Tall2(pygame.sprite.Sprite):
 	def __init__(self, width, height, x, y):
 		super().__init__()
 		self.image = pygame.Surface([width, height])
 		self.image = tall2
 		self.rect = self.image.get_rect()
+		self.rect.x = x 
+		self.rect.y = y
+tall2_group = pygame.sprite.Group()
+tall2_sprite1 = Tall2(125, 196, 0 ,764)
+tall2_sprite2 = Tall2(116, 140, 935, 764)
+tall2_group.add(tall2_sprite1)
+tall2_group.add(tall2_sprite2)
+allBuildings.add(tall2_sprite1)
+allBuildings.add(tall2_sprite2)
+
+class Tall3(pygame.sprite.Sprite):
+	def __init__(self, width, height, x, y):
+		super().__init__()
+		self.image = pygame.Surface([width, height])
+		self.image = tall3
+		self.rect = self.image.get_rect()
+		self.rect.x = x 
+		self.rect.y = y
+tall3_group = pygame.sprite.Group()
+tall3_sprite1 = Tall3(125, 196, 560 ,764)
+tall3_sprite2 = Tall3(116, 140, 1160, 764)
+tall3_group.add(tall3_sprite1)
+tall3_group.add(tall3_sprite2)
+allBuildings.add(tall3_sprite1)
+allBuildings.add(tall3_sprite2)
+
+class Medium1(pygame.sprite.Sprite):
+	def __init__(self, width, height, x, y):
+		super().__init__()
+		self.image = pygame.Surface([width, height])
+		self.image = medium1
+		self.rect = self.image.get_rect()
 		self.rect.x = x
 		self.rect.y = y
+Medium1_group = pygame.sprite.Group()
+Medium1_sprite1 = Medium1(116, 140, 674, 820)
+Medium1_group.add(Medium1_sprite1)
+allBuildings.add(Medium1_sprite1)
 
 class Medium2(pygame.sprite.Sprite):
 	def __init__(self, width, height, x, y):
@@ -211,67 +261,149 @@ allBuildings.add(Medium2_sprite1)
 allBuildings.add(Medium2_sprite2)
 allBuildings.add(Medium2_sprite3)
 
-class building3(pygame.sprite.Sprite):
-	def __init__(self, width, height):
+class Small1(pygame.sprite.Sprite):
+	def __init__(self, width, height, x, y):
 		super().__init__()
-		self.image = small3
-		self.rect = self.image.get_rect()
-
-class building4(pygame.sprite.Sprite):
-	def __init__(self, width, height):
-		super().__init__()
-		self.image = tall1
-		self.rect = self.image.get_rect()
-
-class building5(pygame.sprite.Sprite):
-	def __init__(self, width, height):
-		super().__init__()
+		self.image = pygame.Surface([width, height])
 		self.image = small1
 		self.rect = self.image.get_rect()
+		self.rect.x = x 
+		self.rect.y = y
+small1_group = pygame.sprite.Group()
+small1_sprite1 = Small1(82, 70, 390, 890)
+small1_group.add(small1_sprite1)
+allBuildings.add(small1_sprite1)
 
-class building6(pygame.sprite.Sprite):
-	def __init__(self, width, height):
+class Small2(pygame.sprite.Sprite):
+	def __init__(self, width, height, x, y):
 		super().__init__()
-		self.image = medium2
-		self.rect = self.image.get_rect()
-
-class building7(pygame.sprite.Sprite):
-	def __init__(self, width, height):
-		super().__init__()
-		self.image = tall3
-		self.rect = self.image.get_rect()
-		self.rect.x = 560
-		self.rect.y = 764
-
-class building8(pygame.sprite.Sprite):
-	def __init__(self, width, height):
-		super().__init__()
-		self.image = medium1
-		self.rect = self.image.get_rect()
-
-class building9(pygame.sprite.Sprite):
-	def __init__(self, width, height):
-		super().__init__()
-		self.image = tall1
-		self.rect = self.image.get_rect()
-
-class building10(pygame.sprite.Sprite):
-	def __init__(self, width, height):
-		super().__init__()
+		self.image = pygame.Surface([width, height])
 		self.image = small2
 		self.rect = self.image.get_rect()
+		self.rect.x = x 
+		self.rect.y = y
+small2_group = pygame.sprite.Group()
+small2_sprite1 = Small2(82, 70, 854, 890)
+small2_group.add(small2_sprite1)
+allBuildings.add(small2_sprite1)
 
-class building12(pygame.sprite.Sprite):
-	def __init__(self, width, height):
+class Small3(pygame.sprite.Sprite):
+	def __init__(self, width, height, x, y):
 		super().__init__()
-		self.image = medium2
+		self.image = pygame.Surface([width, height])
+		self.image = small3
 		self.rect = self.image.get_rect()
+		self.rect.x = x 
+		self.rect.y = y
+small3_group = pygame.sprite.Group()
+small3_sprite1 = Small3(97, 77, 236, 860)
+small3_group.add(small3_sprite1)
+allBuildings.add(small3_sprite1)
 
-class building13(pygame.sprite.Sprite):
-	def __init__(self, width, height):
+
+
+# Guns class description
+class Gun(pygame.sprite.Sprite):
+	def __init__(self, width, height, x, y):
 		super().__init__()
-		self.image = tall3
+		self.image = pygame.Surface([width, height])
+		self.image = gun1
+		self.rect  = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+	def update(self):
+ 		self.image = gun2
+gun_group =  pygame.sprite.Group()
+gun_sprite1 = Gun(80, 38, 27, 730)
+gun_sprite2 = Gun(80, 38, 315, 753)
+gun_sprite3 = Gun(80, 38, 780, 753)
+gun_sprite4 = Gun(80, 38, 960, 730)
+gun_sprite5 = Gun(80, 38, 1180, 730)
+gun_group.add(gun_sprite1)
+gun_group.add(gun_sprite2)
+gun_group.add(gun_sprite3)
+gun_group.add(gun_sprite4)
+gun_group.add(gun_sprite5)
+
+
+		# screen.blit(gun1, [27,730])
+		# screen.blit(gun2, [27,730]) 
+
+		# screen.blit(gun1, [315,753])
+		# screen.blit(gun2, [315,753]) 
+
+		
+
+		# screen.blit(gun1, [780,753])
+		# screen.blit(gun2, [780,753]) 
+
+		# screen.blit(gun1, [960,730])
+		# screen.blit(gun2, [960,730]) 
+		
+		# screen.blit(gun1, [1180,730])
+		# screen.blit(gun2, [1180,730]) 
+
+
+
+
+# Bullets
+bullet_group = pygame.sprite.Group()
+class Bullet(pygame.sprite.Sprite):
+	def __init__(self, width, height, x, y, color):
+		super().__init__()
+		self.image = pygame.Surface([width, height])
+		self.image.fill(SILVER)
 		self.rect = self.image.get_rect()
+		self.rect.x = x - 5
+		self.rect.y = y - 5
+	def update(self, x, y):
+		self.rect.y = self.rect.y -10
+		if self.rect.y <= 0:
+			self.rect.x = x
+			self.rect.y = y
+
+bullet_sprite1 = Bullet(5, 5, 27, 715, SILVER)
+bullet_sprite2 = Bullet(5, 5, 315, 715, SILVER)
+bullet_sprite3 = Bullet(5, 5, 780, 715, SILVER)
+bullet_sprite4 = Bullet(5, 5, 960, 715, SILVER)
+bullet_sprite5 = Bullet(5, 5, 1180, 715, SILVER)
+bullet_sprite6 = Bullet(5, 5, 580, 715, SILVER)
+bullet_group.add(bullet_sprite1)
+bullet_group.add(bullet_sprite2)
+bullet_group.add(bullet_sprite3)
+bullet_group.add(bullet_sprite4)
+bullet_group.add(bullet_sprite5)
+bullet_group.add(bullet_sprite6)
+
+
+
+# bullet_group = pygame.sprite.Group()
+# class Bullet(pygame.sprite.Sprite):
+# 	def __init__(self, width, height, radius, x, y, color):
+# 		super().__init__()
+# 		self.image = pygame.Surface([width, height])
+# 		pygame.draw.circle(self.image, color, (x, y), radius, 0)
+# 		self.rect = self.image.get_rect()
+# 		self.rect.x = x - 5
+# 		self.rect.y = y - 5
+# 	def update(self, x, y):
+# 		self.rect.y = self.rect.y -10
+# 		if self.rect.y <= 0:
+# 			self.rect.x = x
+# 			self.rect.y = y
+
+# bullet_sprite1 = Bullet(10, 10, 10, 27, 715, SILVER)
+
+# bullet_group.add(bullet_sprite1)
+
+		
+
+
+# bullet_group = pygame.sprite.Group()
+# for x in range (10):
+# 	bullet_sprite = Bullet(10, 10, SILVER)
+# 	bullet_group.add(bullet_sprite)
+
 
 
 
@@ -308,60 +440,19 @@ def game_loop():
 
 
 		allBuildings.draw(screen)
+		gun_group.draw(screen)
+		# gun_sprite1.draw(screen)
+		bullet_group.draw(screen)
+		bullet_sprite1.update(27, 715)
+		bullet_sprite2.update(315, 715)
+		bullet_sprite3.update(780, 715)
+		bullet_sprite4.update(960, 715)
+		bullet_sprite5.update(1180, 715)
+		bullet_sprite6.update(580, 715)
 
+		# Barrels
+		pygame.draw.rect(screen, GOLDISH, (64,731,10,50))
 
-
-		# Game Assset Placement
-
-		#skyline
-		# screen.blit(tall2,[0,764])			#1 WEAPON
-		#screen.blit(tall2d1,[0,781])
-		#screen.blit(tall2d2,[0,855])
-		
-		# screen.blit(medium2,[120,820])		#2
-		#screen.blit(medium2d1,[120,835])
-		#screen.blit(medium2d2,[120,889])
-		
-		# screen.blit(small3,[235,860])		#3
-		#screen.blit(small3d1,[235,884])
-
-		# screen.blit(tall1, [322,790])		#4 WEAPON
-		#screen.blit(tall1d1, [322,815])
-		#screen.blit(tall1d2, [322,877])
-
-		# screen.blit(small1, [390,890])		#5
-		#screen.blit(small1d1, [390,909])
-
-		# screen.blit(medium2, [470,820 ])	#6
-		#screen.blit(medium2d1, [470,836])
-		#screen.blit(medium2d2, [470,889])
-
-		# screen.blit(tall3,  [560,764])		#7 WEAPON
-		#screen.blit(tall3d1, [560,794])
-		#screen.blit(tall3d2, [560,851])
-
-		# screen.blit(medium1,  [674,820])	#8
-		#screen.blit(medium1d1, [674,835])
-		#screen.blit(medium1d2, [674,889])
-
-		# screen.blit(tall1,  [787,790])		#9 WEAPON
-		#screen.blit(tall1d1, [787,815])
-		#screen.blit(tall1d2, [787,877])
-
-		# screen.blit(small2,[854,890])		#10
-		#screen.blit(small2d1, [854,889])
-
-		# screen.blit(tall2, [935,764])		#11 WEAPON
-		#screen.blit(tall2d1, [935,781])
-		#screen.blit(tall2d2, [935,855])
-
-		# screen.blit(medium2,[1050, 820])	#12
-		#screen.blit(medium2d1, [1050,836])
-		#screen.blit(medium2d2, [1050,889])
-
-		# screen.blit(tall3,[1160, 764] )		#13 WEAPON
-		#screen.blit(tall3d1, [1160,794])
-		#screen.blit(tall3d2, [1160,851])
 
 		#coin
 		screen.blit(fusion , [1120, 20])
@@ -386,32 +477,21 @@ def game_loop():
 		screen.blit(upgrade, [795,805])
 		
 		#Guns
-		screen.blit(gun1, [27,730])
-		screen.blit(gun2, [27,730]) 
-
-		screen.blit(gun1, [315,753])
-		screen.blit(gun2, [315,753]) 
-
-		screen.blit(gun1, [580,726])
+	
+		screen.blit(gun1, [580,726])  # USER CONTROLLED
 		screen.blit(gun2, [580,726]) 
 
-		screen.blit(gun1, [780,753])
-		screen.blit(gun2, [780,753]) 
-
-		screen.blit(gun1, [960,730])
-		screen.blit(gun2, [960,730]) 
-		
-		screen.blit(gun1, [1180,730])
-		screen.blit(gun2, [1180,730]) 
 
 		pygame.display.flip()
 
+		collision = pygame.sprite.groupcollide(meteor1_group, allBuildings, False, True)
 
-		# if (pygame.sprite.collide_rect(Meteor1, building7)):
-		# 	screen.fill(0,0,0)
+		collision = pygame.sprite.groupcollide(meteor2_group, allBuildings, False, True)
 
-		collision = pygame.sprite.groupcollide(meteor1_group, allBuildings, True, True)
-		print(collision)
+		collision = pygame.sprite.groupcollide(meteor3_group, allBuildings, False, True)
+
+		collision = pygame.sprite.groupcollide(meteor4_group, allBuildings, False, True)
+		# print(collision)
 
 		
  
