62c0ceed33adba47c0d38c4f096b7eaf4d900e28
Dev Nahar <Dev@Devs-iMac.local>
2019-01-03 21:52:44 +0000

    Skyline Defence v1.01

--- main.py
+++ main.py
@@ -25,6 +25,142 @@ cursor_img_rect = cursor_img.get_rect()
 pygame.mixer.music.load("music/musicbg.mp3")
 pygame.mixer.music.play(-1, 0.0) 
 
+normal_hit = 1
+upgraded_hit = 2
+
+# Meteor Class Descriptions
+class Meteor1(pygame.sprite.Sprite):
+	# Constructor
+	def __init__(self):
+		super().__init__()
+		self.image = meteor1
+		self.rect = self.image.get_rect()
+		self.rect.x = random.randrange(0, 1200)
+		self.rect.y = random.randrange(-1500, 0)
+		self.speed = 2
+		self.health = 2
+	def update(self):
+		global QUITGAME
+		self.rect.y = self.rect.y + self.speed
+		self.rect.x = self.rect.x + random.randrange(-1,2)
+		if self.rect.y >= 960:
+			game_over()
+	def hit(self):
+		self.health = self.health - normal_hit
+		return self.health
+	def upgraded_hit(self):
+		self.health = self.health - upgraded_hit
+		return self.health
+
+class Meteor2(pygame.sprite.Sprite):
+	# Constructor
+	def __init__(self):
+		super().__init__()
+		self.image = meteor2
+		self.rect = self.image.get_rect()
+		self.rect.x = random.randrange(0, 1200)
+		self.rect.y = random.randrange(-3000,0)
+		self.speed = 1.4
+		self.health = 4
+	def update(self):
+		global QUITGAME
+		self.rect.y = self.rect.y + self.speed
+		self.rect.x = self.rect.x + random.randrange(-1,2)
+		if self.rect.y >= 960:
+			game_over()
+	def hit(self):
+		self.health = self.health - normal_hit
+		return self.health
+	def upgraded_hit(self):
+		self.health = self.health - upgraded_hit
+		return self.health
+	def check_y(self):
+		return self.rect.y
+
+class Meteor3(pygame.sprite.Sprite):
+	def __init__(self):
+		super().__init__()
+		self.image = pygame.Surface([20,52])
+		self.image = meteor3
+		self.rect = self.image.get_rect()
+		self.rect.x = random.randrange(0, 1200)
+		self.rect.y = random.randrange(-1500, 0)
+		self.speed = 3
+		self.health = 1
+	def update(self):
+		global QUITGAME
+		self.rect.y = self.rect.y + self.speed
+		self.rect.x = self.rect.x + random.randrange(-1,2)
+		if self.rect.y >= 960:
+			game_over()
+	def hit(self):
+		self.health = self.health - normal_hit
+		return self.health
+	def upgraded_hit(self):
+		self.health = self.health - upgraded_hit
+		return self.health
+	def check_y(self):
+		return self.rect.y
+
+class Meteor4(pygame.sprite.Sprite):
+	def __init__(self):
+		super().__init__()
+		self.image = pygame.Surface([45,78])
+		self.image = meteor4
+		self.rect = self.image.get_rect()
+		self.rect.x = random.randrange(0, 1200)
+		self.rect.y = random.randrange(-1500,0)
+		self.speed = 1.7
+		self.health = 8
+	def update(self):
+		global QUITGAME
+		self.rect.y = self.rect.y + self.speed
+		self.rect.x = self.rect.x + random.randrange(-1,2)
+		if self.rect.y >= 960:
+			game_over()
+	def hit(self):
+		self.health = self.health - normal_hit
+		return self.health
+	def upgraded_hit(self):
+		self.health = self.health - upgraded_hit
+		return self.health
+	def check_y(self):
+		return self.rect.y
+
+def add_meteor(Type, type_group):
+	meteor_sprite = Type()
+	type_group.add(meteor_sprite)
+	all_meteors_group.add(meteor_sprite)
+
+def game_over():
+	game_over = True
+	global QUITGAME
+	while game_over:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				game_over = False
+				QUITGAME = True
+		screen.blit(game_over_img, [0,0])
+		score_total = scoreFont.render(str((FUSION * 10) + SCORE), True, (255,255,255))
+		screen.blit(score_total, (670, 565))
+		pygame.display.flip()
+
+# Meteor Sprite Creation
+
+all_meteors_group = pygame.sprite.Group()
+meteor1_group = pygame.sprite.Group()
+meteor2_group = pygame.sprite.Group()
+meteor3_group = pygame.sprite.Group()
+meteor4_group = pygame.sprite.Group()
+for x in range (no_Meteor1):
+	add_meteor(Meteor1, meteor1_group)
+for x in range (no_Meteor2):
+	add_meteor(Meteor2, meteor2_group)
+for x in range (no_Meteor3):
+	add_meteor(Meteor3, meteor3_group)
+for x in range (no_Meteor4):
+	add_meteor(Meteor4, meteor4_group)
+
 def game_loop():
 	global QUITGAME, PAUSED, SCORE, FIRED, DELAY, READY, shot, FUSION
 	global no_Meteor1, no_Meteor2, no_Meteor3, no_Meteor4
@@ -316,18 +452,18 @@ def how_to_play():
 					how_to_play = False
 		screen.blit(how_to_play_img, [0,0])
 		pygame.display.flip()
-def game_over():
-	game_over = True
-	global QUITGAME
-	while game_over:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				game_over = False
-				QUITGAME = True
-		screen.blit(game_over_img, [0,0])
-		score_total = scoreFont.render(str((FUSION * 10) + SCORE), True, (255,255,255))
-		screen.blit(score_total, (670, 565))
-		pygame.display.flip()
+# def game_over():
+# 	game_over = True
+# 	global QUITGAME
+# 	while game_over:
+# 		for event in pygame.event.get():
+# 			if event.type == pygame.QUIT:
+# 				game_over = False
+# 				QUITGAME = True
+# 		screen.blit(game_over_img, [0,0])
+# 		score_total = scoreFont.render(str((FUSION * 10) + SCORE), True, (255,255,255))
+# 		screen.blit(score_total, (670, 565))
+# 		pygame.display.flip()
 def pause():
 	global QUITGAME, PAUSED
 	while PAUSED:
@@ -427,6 +563,7 @@ def destroy_defense(gun_hit):
 		GUNS_ACTIVE[POSITIONS[gun]] = False
 		all_guns[gun].kill()
 		all_barrels[gun].kill()
+
 
 
 
--- meteors.py
+++ meteors.py
@@ -4,125 +4,140 @@ import math
 from global_vars import *
 from assets import *
 
-normal_hit = 1
-upgraded_hit = 2
 
-# Meteor Class Descriptions
-class Meteor1(pygame.sprite.Sprite):
-	# Constructor
-	def __init__(self):
-		super().__init__()
-		self.image = meteor1
-		self.rect = self.image.get_rect()
-		self.rect.x = random.randrange(0, 1200)
-		self.rect.y = random.randrange(-1500, 0)
-		self.speed = 2
-		self.health = 2
-	def update(self):
-		global QUITGAME
-		self.rect.y = self.rect.y + self.speed
-		self.rect.x = self.rect.x + random.randrange(-1,2)
-		if self.rect.y >= 960:
-			game_over()
-	def hit(self):
-		self.health = self.health - normal_hit
-		return self.health
-	def upgraded_hit(self):
-		self.health = self.health - upgraded_hit
-		return self.health
+# normal_hit = 1
+# upgraded_hit = 2
 
-class Meteor2(pygame.sprite.Sprite):
-	# Constructor
-	def __init__(self):
-		super().__init__()
-		self.image = meteor2
-		self.rect = self.image.get_rect()
-		self.rect.x = random.randrange(0, 1200)
-		self.rect.y = random.randrange(-3000,0)
-		self.speed = 1.4
-		self.health = 4
-	def update(self):
-		self.rect.y = self.rect.y + self.speed
-		self.rect.x = self.rect.x + random.randrange(-1,2)
-		if self.rect.y >= 960:
-			self.rect.x = random.randrange(0, 1280)
-			self.rect.y = random.randrange(-3000,0)
-	def hit(self):
-		self.health = self.health - normal_hit
-		return self.health
-	def upgraded_hit(self):
-		self.health = self.health - upgraded_hit
-		return self.health
-	def check_y(self):
-		return self.rect.y
+# # Meteor Class Descriptions
+# class Meteor1(pygame.sprite.Sprite):
+# 	# Constructor
+# 	def __init__(self):
+# 		super().__init__()
+# 		self.image = meteor1
+# 		self.rect = self.image.get_rect()
+# 		self.rect.x = random.randrange(0, 1200)
+# 		self.rect.y = random.randrange(-1500, 0)
+# 		self.speed = 2
+# 		self.health = 2
+# 	def update(self):
+# 		global QUITGAME
+# 		self.rect.y = self.rect.y + self.speed
+# 		self.rect.x = self.rect.x + random.randrange(-1,2)
+# 		if self.rect.y >= 960:
+# 			game_over()
+# 	def hit(self):
+# 		self.health = self.health - normal_hit
+# 		return self.health
+# 	def upgraded_hit(self):
+# 		self.health = self.health - upgraded_hit
+# 		return self.health
 
-class Meteor3(pygame.sprite.Sprite):
-	def __init__(self):
-		super().__init__()
-		self.image = pygame.Surface([20,52])
-		self.image = meteor3
-		self.rect = self.image.get_rect()
-		self.rect.x = random.randrange(0, 1200)
-		self.rect.y = random.randrange(-1500, 0)
-		self.speed = 3
-		self.health = 1
-	def update(self):
-		self.rect.y = self.rect.y + self.speed
-		self.rect.x = self.rect.x + random.randrange(-1,2)
-		if self.rect.y >= 960:
-			self.rect.x = random.randrange(0, 1280)
-			self.rect.y = random.randrange(-3000,0)
-	def hit(self):
-		self.health = self.health - normal_hit
-		return self.health
-	def upgraded_hit(self):
-		self.health = self.health - upgraded_hit
-		return self.health
-	def check_y(self):
-		return self.rect.y
+# class Meteor2(pygame.sprite.Sprite):
+# 	# Constructor
+# 	def __init__(self):
+# 		super().__init__()
+# 		self.image = meteor2
+# 		self.rect = self.image.get_rect()
+# 		self.rect.x = random.randrange(0, 1200)
+# 		self.rect.y = random.randrange(-3000,0)
+# 		self.speed = 1.4
+# 		self.health = 4
+# 	def update(self):
+# 		self.rect.y = self.rect.y + self.speed
+# 		self.rect.x = self.rect.x + random.randrange(-1,2)
+# 		if self.rect.y >= 960:
+# 			self.rect.x = random.randrange(0, 1280)
+# 			self.rect.y = random.randrange(-3000,0)
+# 	def hit(self):
+# 		self.health = self.health - normal_hit
+# 		return self.health
+# 	def upgraded_hit(self):
+# 		self.health = self.health - upgraded_hit
+# 		return self.health
+# 	def check_y(self):
+# 		return self.rect.y
 
-class Meteor4(pygame.sprite.Sprite):
-	def __init__(self):
-		super().__init__()
-		self.image = pygame.Surface([45,78])
-		self.image = meteor4
-		self.rect = self.image.get_rect()
-		self.rect.x = random.randrange(0, 1200)
-		self.rect.y = random.randrange(-1500,0)
-		self.speed = 1.7
-		self.health = 8
-	def update(self):
-		self.rect.y = self.rect.y + self.speed
-		self.rect.x = self.rect.x + random.randrange(-1,2)
-		if self.rect.y >= 960:
-			self.rect.x = random.randrange(0, 1280)
-			self.rect.y = random.randrange(-3000,0)
-	def hit(self):
-		self.health = self.health - normal_hit
-		return self.health
-	def upgraded_hit(self):
-		self.health = self.health - upgraded_hit
-		return self.health
-	def check_y(self):
-		return self.rect.y
+# class Meteor3(pygame.sprite.Sprite):
+# 	def __init__(self):
+# 		super().__init__()
+# 		self.image = pygame.Surface([20,52])
+# 		self.image = meteor3
+# 		self.rect = self.image.get_rect()
+# 		self.rect.x = random.randrange(0, 1200)
+# 		self.rect.y = random.randrange(-1500, 0)
+# 		self.speed = 3
+# 		self.health = 1
+# 	def update(self):
+# 		self.rect.y = self.rect.y + self.speed
+# 		self.rect.x = self.rect.x + random.randrange(-1,2)
+# 		if self.rect.y >= 960:
+# 			self.rect.x = random.randrange(0, 1280)
+# 			self.rect.y = random.randrange(-3000,0)
+# 	def hit(self):
+# 		self.health = self.health - normal_hit
+# 		return self.health
+# 	def upgraded_hit(self):
+# 		self.health = self.health - upgraded_hit
+# 		return self.health
+# 	def check_y(self):
+# 		return self.rect.y
 
-def add_meteor(Type, type_group):
-	meteor_sprite = Type()
-	type_group.add(meteor_sprite)
-	all_meteors_group.add(meteor_sprite)
+# class Meteor4(pygame.sprite.Sprite):
+# 	def __init__(self):
+# 		super().__init__()
+# 		self.image = pygame.Surface([45,78])
+# 		self.image = meteor4
+# 		self.rect = self.image.get_rect()
+# 		self.rect.x = random.randrange(0, 1200)
+# 		self.rect.y = random.randrange(-1500,0)
+# 		self.speed = 1.7
+# 		self.health = 8
+# 	def update(self):
+# 		self.rect.y = self.rect.y + self.speed
+# 		self.rect.x = self.rect.x + random.randrange(-1,2)
+# 		if self.rect.y >= 960:
+# 			self.rect.x = random.randrange(0, 1280)
+# 			self.rect.y = random.randrange(-3000,0)
+# 	def hit(self):
+# 		self.health = self.health - normal_hit
+# 		return self.health
+# 	def upgraded_hit(self):
+# 		self.health = self.health - upgraded_hit
+# 		return self.health
+# 	def check_y(self):
+# 		return self.rect.y
 
-# Meteor Sprite Creation
+# def add_meteor(Type, type_group):
+# 	meteor_sprite = Type()
+# 	type_group.add(meteor_sprite)
+# 	all_meteors_group.add(meteor_sprite)
 
-all_meteors_group = pygame.sprite.Group()
-meteor1_group = pygame.sprite.Group()
-meteor2_group = pygame.sprite.Group()
-meteor3_group = pygame.sprite.Group()
-meteor4_group = pygame.sprite.Group()
-for x in range (no_Meteor1):
-	add_meteor(Meteor1, meteor1_group)
-for x in range (no_Meteor2):
-	add_meteor(Meteor2, meteor2_group)
-for x in range (no_Meteor3):
-	add_meteor(Meteor3, meteor3_group)
-for x in range (no_Meteor4):
-	add_meteor(Meteor4, meteor4_group)
+
+# def game_over():
+# 	game_over = True
+# 	global QUITGAME
+# 	while game_over:
+# 		for event in pygame.event.get():
+# 			if event.type == pygame.QUIT:
+# 				game_over = False
+# 				QUITGAME = True
+# 		screen.blit(game_over_img, [0,0])
+# 		score_total = scoreFont.render(str((FUSION * 10) + SCORE), True, (255,255,255))
+# 		screen.blit(score_total, (670, 565))
+# 		pygame.display.flip()
+
+# # Meteor Sprite Creation
+
+# all_meteors_group = pygame.sprite.Group()
+# meteor1_group = pygame.sprite.Group()
+# meteor2_group = pygame.sprite.Group()
+# meteor3_group = pygame.sprite.Group()
+# meteor4_group = pygame.sprite.Group()
+# for x in range (no_Meteor1):
+# 	add_meteor(Meteor1, meteor1_group)
+# for x in range (no_Meteor2):
+# 	add_meteor(Meteor2, meteor2_group)
+# for x in range (no_Meteor3):
+# 	add_meteor(Meteor3, meteor3_group)
+# for x in range (no_Meteor4):
+# 	add_meteor(Meteor4, meteor4_group)
